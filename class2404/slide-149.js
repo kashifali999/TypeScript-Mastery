4a69ab03ea4e5315704f1b074beb2f4a2d814888
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-24 19:14:44 +0500

    initial commit

---
+++ class2404/slide-149.js
@@ -0,0 +1,14 @@
+if (false) {
+    console.log("Welcome");
+}
+else {
+    console.log("You can't enter PN Auditorium");
+}
+//-------------------------------------------------
+var isAssign = true;
+if (isAssign) {
+    console.log("Welcome");
+}
+else {
+    console.log("You can't enter PN Auditorium");
+}
---
+++ class2404/slide-149.ts
@@ -0,0 +1,11 @@
+if (false) {
+    console.log("Welcome");
+}
+else {console.log("You can't enter PN Auditorium")}
+//-------------------------------------------------
+let isAssign = true
+
+if (isAssign) {
+    console.log("Welcome");
+}
+else {console.log("You can't enter PN Auditorium")}
