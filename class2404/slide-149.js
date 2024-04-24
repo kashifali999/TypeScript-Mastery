d01569d09f0af0b18efd9b797b572ee8ab4ea8f9
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-24 20:43:35 +0500

    initial commit

--- class2404/slide-149.js
+++ class2404/slide-149.js
@@ -1,14 +1,22 @@
-if (false) {
-    console.log("Welcome");
+"use strict";
+// if (false) {
+//     console.log("Welcome");
+// }
+// else {console.log("You can't enter PN Auditorium")}
+// //-------------------------------------------------
+// let isAssign = true
+// if (isAssign) {
+//     console.log("Welcome");
+// }
+// else {console.log("You can't enter PN Auditorium")}
+// //-------------------------------------------------
+let cond_1 = false; // Mandatory
+let cond_2 = true; // Optional
+let cond_3 = true; // Optional
+if (cond_1 && (cond_2 || cond_3)) {
+    console.log("Logic true");
 }
 else {
-    console.log("You can't enter PN Auditorium");
+    console.log("Logic false");
 }
 //-------------------------------------------------
-var isAssign = true;
-if (isAssign) {
-    console.log("Welcome");
-}
-else {
-    console.log("You can't enter PN Auditorium");
-}
--- class2404/slide-149.ts
+++ class2404/slide-149.ts
@@ -1,11 +1,22 @@
-if (false) {
-    console.log("Welcome");
-}
-else {console.log("You can't enter PN Auditorium")}
-//-------------------------------------------------
-let isAssign = true
+// if (false) {
+//     console.log("Welcome");
+// }
+// else {console.log("You can't enter PN Auditorium")}
+// //-------------------------------------------------
+// let isAssign = true
 
-if (isAssign) {
-    console.log("Welcome");
+// if (isAssign) {
+//     console.log("Welcome");
+// }
+// else {console.log("You can't enter PN Auditorium")}
+// //-------------------------------------------------
+let cond_1 = false; // Mandatory
+let cond_2 = true;  // Optional
+let cond_3 = true;  // Optional
+
+if (cond_1 && (cond_2 || cond_3)) {
+    console.log("Logic true");
+} else {
+    console.log("Logic false");
 }
-else {console.log("You can't enter PN Auditorium")}
+//-------------------------------------------------
