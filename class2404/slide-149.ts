9491390856667a1d12ba6fe2641eadd338ef8dec
Kashif Ali <kashif_ali999@yahoo.com>
2024-04-24 21:20:48 +0500

    initial commit

--- class2404/slide-149.ts
+++ class2404/slide-149.ts
@@ -10,13 +10,26 @@
 // }
 // else {console.log("You can't enter PN Auditorium")}
 // //-------------------------------------------------
-let cond_1 = false; // Mandatory
-let cond_2 = true;  // Optional
-let cond_3 = true;  // Optional
+// let cond_1 = false; // Mandatory
+// let cond_2 = true;  // Optional
+// let cond_3 = true;  // Optional
 
-if (cond_1 && (cond_2 || cond_3)) {
-    console.log("Logic true");
-} else {
-    console.log("Logic false");
-}
+// if (cond_1 && (cond_2 || cond_3)) {
+//     console.log("Logic true");
+// } else {
+//     console.log("Logic false");
+// }
 //-------------------------------------------------
+let myPercentage = 90;
+
+if ( myPercentage > 80 ) 
+    console.log("your grade is: A+")
+    
+else if ( myPercentage > 70 ) 
+    console.log("your grade is: A");
+
+else if ( myPercentage > 60 ) 
+    console.log("your grade is: B");
+
+else 
+    console.log("fail!!!");
