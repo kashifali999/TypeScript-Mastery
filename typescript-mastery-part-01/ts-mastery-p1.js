0d09abc2fdee1484c3e9a8cb16ba092e1dfab0b0
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-02 22:36:23 +0500

    initial commit

---
+++ typescript-mastery-part-01/ts-mastery-p1.js
@@ -0,0 +1,18 @@
+"use strict";
+// Slide # 01 (47) - Node.js installation - "node -v" - "npm -v"
+// Slide # 01 (54) - Git installation - "git --version"
+// Slide # 01 (59) - TypeScript installation - "tsc --version" - "tsc -v"
+// Slide # 01 (63) - VS Code installation - "code -v"
+// Slide # 01 (70) - TypeScript extension - "JavaScript and TypeScript Nightly"
+// Slide # 01 (74) - "Hello World" program
+console.log("Hello World");
+// Slide # 01 (99) - "https://github.com/panaverse/learn-typescript"
+// Slide # 01 (102) - "Hello World" program by variable
+let message = "Hello World";
+console.log(message);
+// Slide # 01 (105) - "tsc --init"
+// Slide # 01 (111) - "npm init -y"
+// Slide # 01 (114) - Install types for Node.js "npm i @types/node -D"
+// Slide # 01 (116) - Create .gitignore file
+// Slide # 01 (124) - "https://github.com/panaverse/learn-typescript/blob/master/NODE_PROJECTS/getting-started-exercises.md"
+// Slide # 01 (127) - "https://github.com/panaverse/learn-typescript/tree/master/NODE_PROJECTS"
