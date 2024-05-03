bfb3c5424fff917964398da59b0627baf5f34e41
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-03 06:23:27 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-3-43-data-types-in-typescript/main.js
@@ -0,0 +1,6 @@
+// Slide # 02 (46) - Data types in TypeScript
+var studentName = "Kashif Ali";
+var studentAge = 39;
+var isStudent = true;
+var randomValue = "https://github.com/kashifali999";
+console.log("My name is ".concat(studentName, ". I am ").concat(studentAge, " years old.\n    My current status as a student is ").concat(isStudent, " and my GitHub link is ").concat(randomValue, "."));
---
+++ typescript-mastery-part-02/P-3-43-data-types-in-typescript/main.ts
@@ -0,0 +1,8 @@
+// Slide # 02 (46) - Data types in TypeScript
+let studentName:string = "Kashif Ali";
+let studentAge:number = 39;
+let isStudent:boolean = true;
+let randomValue:any = "https://github.com/kashifali999";
+
+console.log(`My name is ${studentName}. I am ${studentAge} years old.
+    My current status as a student is ${isStudent} and my GitHub link is ${randomValue}.`);
