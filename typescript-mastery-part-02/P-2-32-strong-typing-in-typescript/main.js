ec1b2a7ba37d4637c7c178abd4541ad7a392bf20
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-03 05:45:12 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-2-32-strong-typing-in-typescript/main.js
@@ -0,0 +1,3 @@
+// Slide # 02 (36) - Strong typing in TypeScript
+var userName = "Kashif Ali";
+//userName = 25
---
+++ typescript-mastery-part-02/P-2-32-strong-typing-in-typescript/main.ts
@@ -0,0 +1,3 @@
+// Slide # 02 (36) - Strong typing in TypeScript
+let userName:string = "Kashif Ali"
+//userName = 25
