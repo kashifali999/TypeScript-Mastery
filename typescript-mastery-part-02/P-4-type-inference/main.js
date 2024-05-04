9ff505d5c8bc662de9dc76bfd452a129c4506c6a
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-04 22:32:02 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-4-type-inference/main.js
@@ -0,0 +1,8 @@
+// Strongly typed syntax
+var a = "Kashif Ali";
+var b = 39;
+var c = true;
+// Type inference
+var x = "Kashif Ali";
+var y = 39;
+var z = true;
---
+++ typescript-mastery-part-02/P-4-type-inference/main.ts
@@ -0,0 +1,9 @@
+// Strongly typed syntax
+let a:string = "Kashif Ali"
+let b:number = 39
+let c:boolean = true
+
+// Type inference
+let x = "Kashif Ali"
+let y = 39
+let z = true
