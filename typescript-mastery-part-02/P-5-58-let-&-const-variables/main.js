c93d40da2d99510e290fc8ad22b2345bec9f0b51
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-04 22:49:12 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-5-58-let-&-const-variables/main.js
@@ -0,0 +1,4 @@
+var accountBalance = 10000;
+var accountNumber = 123456789;
+accountBalance = 5000;
+// accountNumber = 0123456789
---
+++ typescript-mastery-part-02/P-5-58-let-&-const-variables/main.ts
@@ -0,0 +1,5 @@
+let accountBalance = 10000
+const accountNumber = 123456789
+
+accountBalance = 5000
+// accountNumber = 0123456789
