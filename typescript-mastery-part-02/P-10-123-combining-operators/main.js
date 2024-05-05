7c9a890a140db43cceb04580cd9d990189da2098
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-06 01:03:02 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-10-123-combining-operators/main.js
@@ -0,0 +1,7 @@
+// Slide # 02 (124) - Combining operators
+var result = 3 + 4 * 5;
+console.log(result);
+// Slide # 02 (125) - Combining operators
+var result2 = 3 + 4 * 5;
+result2++;
+console.log(result2);
---
+++ typescript-mastery-part-02/P-10-123-combining-operators/main.ts
@@ -0,0 +1,7 @@
+// Slide # 02 (124) - Combining operators
+let result:number = 3 + 4 * 5
+console.log(result)
+// Slide # 02 (125) - Combining operators
+let result2:number = 3 + 4 * 5
+result2++
+console.log(result2)
