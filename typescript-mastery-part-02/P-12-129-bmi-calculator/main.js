08205327e06eeb0dc9df595ced84a1c69668fb76
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-07 23:35:57 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-12-129-bmi-calculator/main.js
@@ -0,0 +1,4 @@
+var weightInKg = 80;
+var heightInMeters = 1.78;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(bmi);
---
+++ typescript-mastery-part-02/P-12-129-bmi-calculator/main.ts
@@ -0,0 +1,4 @@
+let weightInKg:number = 80
+let heightInMeters:number = 1.78
+let bmi:number = weightInKg / (heightInMeters * heightInMeters)
+console.log(bmi)
