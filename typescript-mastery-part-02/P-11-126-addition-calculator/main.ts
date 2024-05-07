d5b7fa76717e38ffebabfea7b429cf2420b9c1d9
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-07 23:23:08 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-11-126-addition-calculator/main.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer"
+let input1 = await inquirer.prompt(
+    {name:"num1", type:"number", message:"Enter first number"})
+
+let input2 = await inquirer.prompt(
+    {name:"num2", type:"number", message:"Enter second number"})
+
+let total:number = input1.num1 + input2.num2
+console.log(total)
