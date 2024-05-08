51bbb30f8406824104a03bd6aa7881cc70cc9b09
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-09 00:02:22 +0500

    initial commit

--- typescript-mastery-part-02/P-13-131-home-work-calculator/main.js
+++ typescript-mastery-part-02/P-13-131-home-work-calculator/main.js
@@ -1,8 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var result = await inquirer_1.default.prompt([
+import inquierer from "inquirer";
+let result = await inquierer.prompt([
     { name: "firstNumber", type: "number", message: "Enter first number" },
     { name: "secondNumber", type: "number", message: "Enter second number" },
     { name: "operator", type: "list", message: "Select operator", choices: ["Add", "Subtract", "Multiply", "Divide", "Exponentiation", "Modulus", "BMI"] }
 ]);
+if (result.operator === "Add") {
+    console.log(result.firstNumber + result.secondNumber);
+}
+else if (result.operator === "Subtract") {
+    console.log(result.firstNumber - result.secondNumber);
+}
+else if (result.operator === "Multiply") {
+    console.log(result.firstNumber * result.secondNumber);
+}
+else if (result.operator === "Divide") {
+    console.log(result.firstNumber / result.secondNumber);
+}
+else if (result.operator === "Exponentiation") {
+    console.log(result.firstNumber ** result.secondNumber);
+}
+else if (result.operator === "Modulus") {
+    console.log(result.firstNumber % result.secondNumber);
+}
+else if (result.operator === "BMI") {
+    console.log(result.firstNumber / (result.secondNumber * result.secondNumber));
+}
