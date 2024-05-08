98277f2d60bc9bfedbd14576607c461465b5cfe7
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-08 23:51:24 +0500

    initial commit

--- typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
+++ typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
@@ -5,3 +5,12 @@ let result = await inquierer.prompt([
     {name:"operator", type:"list", message:"Select operator", choices:["Add", "Subtract", "Multiply", "Divide", "Exponentiation", "Modulus", "BMI"]}
 ])
 
+if      (result.operator === "Add")              {result.firstNumber + result.secondNumber}
+else if (result.operator === "Subtract")         {result.firstNumber - result.secondNumber}
+else if (result.operator === "Multiply")         {result.firstNumber * result.secondNumber}
+else if (result.operator === "Divide")           {result.firstNumber / result.secondNumber}
+else if (result.operator === "Exponentiation")   {result.firstNumber ** result.secondNumber}
+else if (result.operator === "Modulus")          {result.firstNumber % result.secondNumber}
+else if (result.operator === "BMI")              {result.firstNumber % result.secondNumber}
+
+
