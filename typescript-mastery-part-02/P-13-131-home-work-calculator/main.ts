2e437f1b8475e75d444a3248982d51bb25ed13d4
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-12 23:09:35 +0500

    initial commit

--- typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
+++ typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
@@ -26,30 +26,3 @@ else                                                          {let bmi = await i
     else if (selCal.calculationType.basicCal === "Divide")           {console.log(selCal.calculationType.basicCal.firstNumber / selCal.calculationType.basicCal.secondNumber)}
     else if (selCal.calculationType.advCal === "Exponentiation")     {console.log(selCal.calculationType.advCal.firstNumber ** selCal.calculationType.advCal.secondNumber)}
     else if (selCal.calculationType.advCal === "Modulus")            {console.log(selCal.calculationType.advCal.firstNumber % selCal.calculationType.advCal.secondNumber)}
-
-    
-
-/*
-import inquierer from "inquirer"
-let result = await inquierer.prompt([
-    {name:"firstNumber", type:"number", message:"Enter first number"},
-    {name:"secondNumber", type:"number", message:"Enter second number"},
-    {name:"operator", type:"list", message:"Select operator", choices:["Add", "Subtract", "Multiply", "Divide", "Exponentiation", "Modulus", "BMI"]}
-])
-
-if      (result.operator === "Add")              {console.log(result.firstNumber + result.secondNumber)}
-else if (result.operator === "Subtract")         {console.log(result.firstNumber - result.secondNumber)}
-else if (result.operator === "Multiply")         {console.log(result.firstNumber * result.secondNumber)}
-else if (result.operator === "Divide")           {console.log(result.firstNumber / result.secondNumber)}
-else if (result.operator === "Exponentiation")   {console.log(result.firstNumber ** result.secondNumber)}
-else if (result.operator === "Modulus")          {console.log(result.firstNumber % result.secondNumber)}
-//else if (result.operator === "BMI")              {console.log(result.firstNumber / (result.secondNumber * result.secondNumber))}
-
-else if (result.operator === "BMI")              {let calBmi = await inquierer.prompt([
-    {name:"weightInKg", type:"number", message:"Enter weight in kg"},
-    {name:"heightInMeters", type:"number", message:"Enter height in meters"}])
-
-    {console.log(calBmi.weightInKg / (calBmi.heightInMeters * calBmi.heightInMeters))}
-
-}
-*/
