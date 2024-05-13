d1becb52657e87cd7ac01b9c713a2c28d973e6e2
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-13 23:49:53 +0500

    initial commit

--- typescript-mastery-part-02/P-13-131-home-work-calculator/main.js
+++ typescript-mastery-part-02/P-13-131-home-work-calculator/main.js
@@ -1,67 +1,105 @@
-import inquierer from "inquirer";
-let selCal = await inquierer.prompt({ name: "calculationType", type: "list", message: "Select operator", choices: ["Basic Calculations", "Advance Calculations", "BMI"] });
-if (selCal.calculationType === "Basic Calculations") {
-    let basicCal = await inquierer.prompt([
-        { name: "firstNumber", type: "number", message: "Enter first number" },
-        { name: "secondNumber", type: "number", message: "Enter second number" },
-        { name: "operator", type: "list", message: "Select operator", choices: ["Add", "Subtract", "Multiply", "Divide"] }
+import inquirer from "inquirer";
+async function calculation() {
+    const calculationType = await inquirer.prompt([
+        {
+            name: "calculationType",
+            type: "list",
+            choices: ["basic Calculation", "Advance Calculation",],
+            message: "Select the Calculation Type"
+        }
     ]);
-}
-else if (selCal.calculationType === "Advance Calculations") {
-    let advCal = await inquierer.prompt([
-        { name: "firstNumber", type: "number", message: "Enter first number" },
-        { name: "secondNumber", type: "number", message: "Enter second number" },
-        { name: "operator", type: "list", message: "Select operator", choices: ["Exponentiation", "Modulus"] }
-    ]);
-}
-else {
-    let bmi = await inquierer.prompt([
-        { name: "weightInKg", type: "number", message: "Enter weight in kg" },
-        { name: "heightInMeters", type: "number", message: "Enter height in meters" }
-    ]);
-    {
-        console.log(bmi.weightInKg / (bmi.heightInMeters * bmi.heightInMeters));
+    let result;
+    if (calculationType.calculationType === "basic Calculation") {
+        const basicQ = await inquirer.prompt([
+            {
+                name: "num1",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "num2",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "operator",
+                type: "list",
+                choices: ["Addition", "Subtraction", "Multiplication", "division"],
+                message: "Select the operator"
+            }
+        ]);
+        switch (basicQ.operator) {
+            case "Addition":
+                result = basicQ.num1 + basicQ.num2;
+                break;
+            case "Subtraction":
+                result = basicQ.num1 - basicQ.num2;
+                break;
+            case "Multiplication":
+                result = basicQ.num1 * basicQ.num2;
+                break;
+            case "division":
+                result = basicQ.num1 / basicQ.num2;
+                break;
+        }
     }
+    else if (calculationType.calculationType === "Advance Calculation") {
+        const AdvanceQ = await inquirer.prompt([
+            {
+                name: "eNum1",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "eNum2",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "eOperator",
+                type: "list",
+                choices: ["Exponentiation", "Modulus"],
+                message: "Select the operator"
+            }
+        ]);
+        switch (AdvanceQ.eOperator) {
+            case "Exponentiation":
+                result = AdvanceQ.num1 ** AdvanceQ.num2;
+                break;
+            case "Modulus":
+                result = AdvanceQ.num1 % AdvanceQ.num2;
+                break;
+        }
+    }
+    console.log(result);
 }
-if (selCal.calculationType.basicCal === "Add") {
-    console.log(selCal.basicCal.firstNumber + selCal.calculationType.basicCal.secondNumber);
-}
-else if (selCal.calculationType.basicCal === "Subtract") {
-    console.log(selCal.calculationType.basicCal.firstNumber - selCal.calculationType.basicCal.secondNumber);
-}
-else if (selCal.calculationType.basicCal === "Multiply") {
-    console.log(selCal.calculationType.basicCal.firstNumber * selCal.calculationType.basicCal.secondNumber);
-}
-else if (selCal.calculationType.basicCal === "Divide") {
-    console.log(selCal.calculationType.basicCal.firstNumber / selCal.calculationType.basicCal.secondNumber);
-}
-else if (selCal.calculationType.advCal === "Exponentiation") {
-    console.log(selCal.calculationType.advCal.firstNumber ** selCal.calculationType.advCal.secondNumber);
-}
-else if (selCal.calculationType.advCal === "Modulus") {
-    console.log(selCal.calculationType.advCal.firstNumber % selCal.calculationType.advCal.secondNumber);
-}
-/*
-import inquierer from "inquirer"
-let result = await inquierer.prompt([
+calculation();
+/*import inquierer from "inquirer"
+let selCal = await inquierer.prompt(
+    {name:"calculationType", type:"list", message:"Select operator", choices:["Basic Calculations", "Advance Calculations", "BMI"]}
+)
+
+if      (selCal.calculationType === "Basic Calculations")     {let basicCal = await inquierer.prompt([
     {name:"firstNumber", type:"number", message:"Enter first number"},
     {name:"secondNumber", type:"number", message:"Enter second number"},
-    {name:"operator", type:"list", message:"Select operator", choices:["Add", "Subtract", "Multiply", "Divide", "Exponentiation", "Modulus", "BMI"]}
-])
-
-if      (result.operator === "Add")              {console.log(result.firstNumber + result.secondNumber)}
-else if (result.operator === "Subtract")         {console.log(result.firstNumber - result.secondNumber)}
-else if (result.operator === "Multiply")         {console.log(result.firstNumber * result.secondNumber)}
-else if (result.operator === "Divide")           {console.log(result.firstNumber / result.secondNumber)}
-else if (result.operator === "Exponentiation")   {console.log(result.firstNumber ** result.secondNumber)}
-else if (result.operator === "Modulus")          {console.log(result.firstNumber % result.secondNumber)}
-//else if (result.operator === "BMI")              {console.log(result.firstNumber / (result.secondNumber * result.secondNumber))}
-
-else if (result.operator === "BMI")              {let calBmi = await inquierer.prompt([
+    {name:"operator", type:"list", message:"Select operator", choices:["Add", "Subtract", "Multiply", "Divide"]}
+])}
+else if (selCal.calculationType === "Advance Calculations")   {let advCal = await inquierer.prompt([
+    {name:"firstNumber", type:"number", message:"Enter first number"},
+    {name:"secondNumber", type:"number", message:"Enter second number"},
+    {name:"operator", type:"list", message:"Select operator", choices:["Exponentiation", "Modulus"]}
+])}
+else                                                          {let bmi = await inquierer.prompt([
     {name:"weightInKg", type:"number", message:"Enter weight in kg"},
     {name:"heightInMeters", type:"number", message:"Enter height in meters"}])
 
-    {console.log(calBmi.weightInKg / (calBmi.heightInMeters * calBmi.heightInMeters))}
+    {console.log(bmi.weightInKg / (bmi.heightInMeters * bmi.heightInMeters))}}
 
-}
-*/ 
+
+    if      (selCal.calculationType.basicCal === "Add")              {console.log(selCal.basicCal.firstNumber + selCal.calculationType.basicCal.secondNumber)}
+    else if (selCal.calculationType.basicCal === "Subtract")         {console.log(selCal.calculationType.basicCal.firstNumber - selCal.calculationType.basicCal.secondNumber)}
+    else if (selCal.calculationType.basicCal === "Multiply")         {console.log(selCal.calculationType.basicCal.firstNumber * selCal.calculationType.basicCal.secondNumber)}
+    else if (selCal.calculationType.basicCal === "Divide")           {console.log(selCal.calculationType.basicCal.firstNumber / selCal.calculationType.basicCal.secondNumber)}
+    else if (selCal.calculationType.advCal === "Exponentiation")     {console.log(selCal.calculationType.advCal.firstNumber ** selCal.calculationType.advCal.secondNumber)}
+    else if (selCal.calculationType.advCal === "Modulus")            {console.log(selCal.calculationType.advCal.firstNumber % selCal.calculationType.advCal.secondNumber)}
+    */ 
--- typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
+++ typescript-mastery-part-02/P-13-131-home-work-calculator/main.ts
@@ -1,4 +1,109 @@
-import inquierer from "inquirer"
+import inquirer from "inquirer"
+async function calculation() {
+    const calculationType: any = await inquirer.prompt([
+        {
+            name: "calculationType",
+            type: "list",
+            choices: ["basic Calculation", "Advance Calculation",],
+            message: "Select the Calculation Type"
+        }
+    ])
+    let result;
+
+    if (calculationType.calculationType === "basic Calculation") {
+        const basicQ = await inquirer.prompt([
+            {
+                name: "num1",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "num2",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "operator",
+                type: "list",
+                choices: ["Addition", "Subtraction", "Multiplication", "division"],
+                message: "Select the operator"
+            }
+        ])
+        switch (basicQ.operator) {
+            case "Addition":
+                result = basicQ.num1 + basicQ.num2;
+                break;
+
+            case "Subtraction":
+                result = basicQ.num1 - basicQ.num2
+                break;
+            case "Multiplication":
+                result = basicQ.num1 * basicQ.num2
+                break;
+            case "division":
+                result = basicQ.num1 / basicQ.num2
+                break;
+        }
+        
+
+    } else if (calculationType.calculationType === "Advance Calculation") {
+        const AdvanceQ = await inquirer.prompt([
+            {
+                name: "eNum1",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "eNum2",
+                type: "number",
+                message: "Type the value"
+            },
+            {
+                name: "eOperator",
+                type: "list",
+                choices: ["Exponentiation", "Modulus"],
+                message: "Select the operator"
+            }
+        ])
+        switch (AdvanceQ.eOperator) {
+            case "Exponentiation":
+                result = AdvanceQ.num1 ** AdvanceQ.num2;
+                break;
+
+            case "Modulus":
+                result = AdvanceQ.num1 % AdvanceQ.num2
+                break;
+            }
+        }
+            console.log(result);
+            
+    }
+calculation()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import inquierer from "inquirer"
 let selCal = await inquierer.prompt(
     {name:"calculationType", type:"list", message:"Select operator", choices:["Basic Calculations", "Advance Calculations", "BMI"]}
 )
@@ -26,3 +131,4 @@ else                                                          {let bmi = await i
     else if (selCal.calculationType.basicCal === "Divide")           {console.log(selCal.calculationType.basicCal.firstNumber / selCal.calculationType.basicCal.secondNumber)}
     else if (selCal.calculationType.advCal === "Exponentiation")     {console.log(selCal.calculationType.advCal.firstNumber ** selCal.calculationType.advCal.secondNumber)}
     else if (selCal.calculationType.advCal === "Modulus")            {console.log(selCal.calculationType.advCal.firstNumber % selCal.calculationType.advCal.secondNumber)}
+    */
