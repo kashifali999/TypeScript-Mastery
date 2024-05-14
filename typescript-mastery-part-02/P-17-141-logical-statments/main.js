21388be25b729277a47c45aac7ca874b17f946ae
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-14 23:55:55 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-17-141-logical-statments/main.js
@@ -0,0 +1,15 @@
+/*let isRaining = false
+
+if(isRaining){console.log("Wear a raincoat")}
+else{console.log("Wear sunglasses")}*/
+//--------------------------------------------------------------------------
+var weather = "cloudy";
+if (weather == "raining") {
+    console.log("Wear a raincoat");
+}
+else if (weather == "cloudy") {
+    console.log("Wear light jacket");
+}
+else {
+    console.log("Wear sunglasses");
+}
---
+++ typescript-mastery-part-02/P-17-141-logical-statments/main.ts
@@ -0,0 +1,12 @@
+/*let isRaining = false
+
+if(isRaining){console.log("Wear a raincoat")}
+else{console.log("Wear sunglasses")}*/
+//--------------------------------------------------------------------------
+let weather = "cloudy"
+
+if(weather == "raining")
+    {console.log("Wear a raincoat")}
+else if(weather == "cloudy")
+    {console.log("Wear light jacket")}
+else{console.log("Wear sunglasses")}
