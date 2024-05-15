10ffed73d793dae62d8aeb831242f97e71e55c72
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-15 23:56:04 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-19-153-switch-statment/main.ts
@@ -0,0 +1,12 @@
+let dayOff = "Sunday"
+switch (dayOff){
+    case "Saturday":
+        console.log("Go for hiking")
+        break
+    case "Sunday":
+        console.log("Read a book")
+        break
+    default:
+        console.log("Work on hobby")
+    
+}
