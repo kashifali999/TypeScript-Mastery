d0eeabcc77ad9fbd7a6a0f750633c898685f95ef
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-19 22:57:08 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-21-159-evaluating-number-game/main.js
@@ -0,0 +1,11 @@
+var guess = 7;
+var target = 5;
+if (guess < target) {
+    console.log("Your guess is too low");
+}
+else if (guess > target) {
+    console.log("Your guess is too high");
+}
+else {
+    console.log("You guessed correcrtly");
+}
