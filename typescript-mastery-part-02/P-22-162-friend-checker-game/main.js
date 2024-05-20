45e7ccf46b794a4965ddfd678a278b400a25a9d8
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-20 23:58:32 +0500

    initial commit

--- typescript-mastery-part-02/P-22-162-friend-checker-game/main.js
+++ typescript-mastery-part-02/P-22-162-friend-checker-game/main.js
@@ -1,7 +1,7 @@
-var isFriend = "Ameen";
+var isFriend = "Kashif";
 if (isFriend === "Ameen" || isFriend === "Alam") {
     console.log("".concat(isFriend, " is your friend"));
 }
 else {
-    "".concat(isFriend, " is not your ffriend");
+    "".concat(isFriend, " is not your friend");
 }
--- typescript-mastery-part-02/P-22-162-friend-checker-game/main.ts
+++ typescript-mastery-part-02/P-22-162-friend-checker-game/main.ts
@@ -1,3 +1,3 @@
-let isFriend:string = "Ameen"
+let isFriend:string = "Kashif"
 if(isFriend === "Ameen" || isFriend === "Alam"){console.log(`${isFriend} is your friend`)}
-else{`${isFriend} is not your ffriend`}
+else{`${isFriend} is not your friend`}
