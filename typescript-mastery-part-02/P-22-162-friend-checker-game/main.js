3bc512b6a2ac02c793c8efed12eba34e6b2d45ca
Kashif Ali <104266609+kashifali999@users.noreply.github.com>
2024-05-25 23:31:09 +0500

    Delete typescript-mastery-part-02/P-22-162-friend-checker-game/main.js

--- typescript-mastery-part-02/P-22-162-friend-checker-game/main.js
+++
@@ -1,7 +0,0 @@
-var isFriend = "Kashif Ali";
-if (isFriend === "Ameen" || isFriend === "Alam") {
-    console.log("".concat(isFriend, " is your friend"));
-}
-else {
-    console.log("".concat(isFriend, " is not your friend"));
-}
