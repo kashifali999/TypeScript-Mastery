504d49054bf6571be14fa053c941a306457a5607
Kashif Ali <kashif_ali999@yahoo.com>
2024-05-27 23:05:56 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-23-166-friend-checker-game-inquirer/main.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+let isFriend = await inquirer.prompt({
+    name: "Friend", type: "input", message: "Enter friend name"
+});
+if (isFriend.Friend === "Kashif" || isFriend.Friend === "Ali") {
+    console.log(`${isFriend.Friend} is your friend`);
+}
+else {
+    {
+        console.log(`${isFriend.Friend} is not your friend`);
+    }
+}
--- typescript-mastery-part-02/P-23-166-friend-checker-game-inquirer/main.ts
+++ typescript-mastery-part-02/P-23-166-friend-checker-game-inquirer/main.ts
@@ -1,1 +1,7 @@
 import inquirer from "inquirer"
+let isFriend = await inquirer.prompt({
+    name : "Friend", type : "input", message : "Enter friend name"
+})
+if(isFriend.Friend === "Kashif" || isFriend.Friend === "Ali")
+    {console.log(`${isFriend.Friend} is your friend`)}
+else{{console.log(`${isFriend.Friend} is not your friend`)}}
