ecccfe6c2ba1c502d858aa35908774439c0dc5de
Kashif Ali <kashif_ali999@yahoo.com>
2024-06-01 23:33:46 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-25-171-home-work-calculator/main.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+let result = await inquirer.prompt([
+    { name: "firstNumber", type: "number", message: "Enter first number" },
+    { name: "secondNumber", type: "number", message: "Enter second number" },
+    { name: "operator", type: "list", message: "Enter operator", choices: ["Add", "Subtract", "Multiply", "Divide"] }
+]);
+if (result.operator === "Add") {
+    console.log("Your result is " + (result.firstNumber + result.secondNumber));
+}
+else if (result.operator === "Subtract") {
+    console.log("Your result is " + (result.firstNumber - result.secondNumber));
+}
+else if (result.operator === "Multiply") {
+    console.log("Your result is " + (result.firstNumber * result.secondNumber));
+}
+else {
+    console.log("Your result is " + (result.firstNumber / result.secondNumber));
+}
