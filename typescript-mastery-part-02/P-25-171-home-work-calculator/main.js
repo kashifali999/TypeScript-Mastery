86e84a890ad1ecdafc39876705c87b010898ee82
Kashif Ali <kashif_ali999@yahoo.com>
2024-06-06 09:01:46 +0500

    initial commit

--- typescript-mastery-part-02/P-25-171-home-work-calculator/main.js
+++ typescript-mastery-part-02/P-25-171-home-work-calculator/main.js
@@ -1,18 +1,20 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 let result = await inquirer.prompt([
-    { name: "firstNumber", type: "number", message: "Enter first number" },
-    { name: "secondNumber", type: "number", message: "Enter second number" },
+    { name: "firstNumber", type: "number", message: chalk.yellowBright("\n\tEnter first number") },
+    { name: "secondNumber", type: "number", message: chalk.yellow("\n\tEnter second number") },
     { name: "operator", type: "list", message: "Enter operator", choices: ["Add", "Subtract", "Multiply", "Divide"] }
 ]);
 if (result.operator === "Add") {
-    console.log("Your result is " + (result.firstNumber + result.secondNumber));
+    console.log(chalk.green("\n\tYour result is " + (result.firstNumber + result.secondNumber)));
 }
 else if (result.operator === "Subtract") {
-    console.log("Your result is " + (result.firstNumber - result.secondNumber));
+    console.log(chalk.blue("\n\tYour result is " + (result.firstNumber - result.secondNumber)));
 }
 else if (result.operator === "Multiply") {
-    console.log("Your result is " + (result.firstNumber * result.secondNumber));
+    console.log(chalk.red("\n\tYour result is " + (result.firstNumber * result.secondNumber)));
 }
 else {
-    console.log("Your result is " + (result.firstNumber / result.secondNumber));
+    console.log(chalk.bgGray("\n\tYour result is " + (result.firstNumber / result.secondNumber)));
 }
+//End
