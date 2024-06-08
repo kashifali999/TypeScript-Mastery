7fa334cdfbca1f3ff87765e6341ddf8586c17e81
Kashif Ali <kashif_ali999@yahoo.com>
2024-06-09 00:00:54 +0500

    initial commit

--- typescript-mastery-part-02/P-25-171-home-work-calculator/main.js
+++ typescript-mastery-part-02/P-25-171-home-work-calculator/main.js
@@ -6,15 +6,15 @@ let result = await inquirer.prompt([
     { name: "operator", type: "list", message: "Enter operator", choices: ["Add", "Subtract", "Multiply", "Divide"] }
 ]);
 if (result.operator === "Add") {
-    console.log("Your result is " + (result.firstNumber + result.secondNumber));
+    console.log(chalk.green("\n\tYour result is " + (result.firstNumber + result.secondNumber)));
 }
 else if (result.operator === "Subtract") {
-    console.log("Your result is " + (result.firstNumber - result.secondNumber));
+    console.log(chalk.green("\n\tYour result is " + (result.firstNumber - result.secondNumber)));
 }
 else if (result.operator === "Multiply") {
-    console.log("Your result is " + (result.firstNumber * result.secondNumber));
+    console.log(chalk.green("\n\tYour result is " + (result.firstNumber * result.secondNumber)));
 }
 else {
-    console.log("Your result is " + (result.firstNumber / result.secondNumber));
+    console.log(chalk.green("\n\tYour result is " + (result.firstNumber / result.secondNumber)));
 }
 //End
--- typescript-mastery-part-02/P-25-171-home-work-calculator/main.ts
+++ typescript-mastery-part-02/P-25-171-home-work-calculator/main.ts
@@ -6,8 +6,8 @@ let result = await inquirer.prompt([
     {name : "operator", type : "list", message : "Enter operator", choices : ["Add", "Subtract", "Multiply", "Divide"]}
 ])
 
-if      (result.operator === "Add")         {console.log("Your result is " + (result.firstNumber + result.secondNumber))}
-else if (result.operator === "Subtract")    {console.log("Your result is " + (result.firstNumber - result.secondNumber))}
-else if (result.operator === "Multiply")    {console.log("Your result is " + (result.firstNumber * result.secondNumber))}
-else                                        {console.log("Your result is " + (result.firstNumber / result.secondNumber))}
+if      (result.operator === "Add")         {console.log(chalk.green("\n\tYour result is " + (result.firstNumber + result.secondNumber)))}
+else if (result.operator === "Subtract")    {console.log(chalk.green("\n\tYour result is " + (result.firstNumber - result.secondNumber)))}
+else if (result.operator === "Multiply")    {console.log(chalk.green("\n\tYour result is " + (result.firstNumber * result.secondNumber)))}
+else                                        {console.log(chalk.green("\n\tYour result is " + (result.firstNumber / result.secondNumber)))}
 //End
