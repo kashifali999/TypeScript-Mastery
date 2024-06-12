7056f7051c46d1576f84eadeba25312590e25e08
Kashif Ali <kashif_ali999@yahoo.com>
2024-06-13 00:25:32 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-28-203-hoisting/main.js
@@ -0,0 +1,6 @@
+//206-Anonymous functions
+var halfFryEgg = function () {
+    var cocked = 1 + 1.5 + 3;
+    console.log(cocked);
+};
+halfFryEgg();
---
+++ typescript-mastery-part-02/P-28-203-hoisting/main.ts
@@ -0,0 +1,6 @@
+//206-Anonymous functions
+let halfFryEgg = function(){
+    let cocked = 1 + 1.5 + 3
+    console.log(cocked)    
+}
+halfFryEgg()
