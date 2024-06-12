6200be0c3a0d5f4ad195099254c2e01909c846a5
Kashif Ali <kashif_ali999@yahoo.com>
2024-06-13 00:03:36 +0500

    initial commit

---
+++ typescript-mastery-part-02/P-27-199-variable-scope/main.js
@@ -0,0 +1,6 @@
+var globalVar = "This is my global variable which is accessible everywhere";
+function showExemple() {
+    var localVar = "This is my local variable which is accessible within function";
+    console.log(globalVar);
+}
+//console.log(localVar)
---
+++ typescript-mastery-part-02/P-27-199-variable-scope/main.ts
@@ -0,0 +1,6 @@
+let globalVar:string = "This is my global variable which is accessible everywhere"
+function showExemple(){
+    let localVar:string = "This is my local variable which is accessible within function"
+    console.log(globalVar)    
+}
+//console.log(localVar)
